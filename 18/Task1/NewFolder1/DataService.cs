d77877af5307c9cfeb4727bc18f59acc162d7e3f
educationalPractice-41TP <lebedevicevgenij374@gmail.com>
2025-04-07 11:46:19 +0300

    lr18

---
+++ 18/Task1/NewFolder1/DataService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StudentDiary
+{
+
+    public class AssignmentRecord
+    {
+        public string AssignmentName { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class ScheduleRecord
+    {
+        public string Subject { get; set; }
+        public DateTime ClassTime { get; set; }
+    }
+
+    public class Diary
+    {
+        public List<GradeRecord> Grades { get; set; } = new List<GradeRecord>();
+        public List<AssignmentRecord> Assignments { get; set; } = new List<AssignmentRecord>();
+        public List<ScheduleRecord> Schedule { get; set; } = new List<ScheduleRecord>();
+    }
+
+    public class DataService
+    {
+        private const string DiaryFile = "diary.json";
+
+        public static void SaveDiaryData(Diary diary)
+        {
+            string jsonData = JsonConvert.SerializeObject(diary, Formatting.Indented);
+            File.WriteAllText(DiaryFile, jsonData);
+        }
+
+        public static Diary LoadDiaryData()
+        {
+            if (File.Exists(DiaryFile))
+            {
+                string jsonData = File.ReadAllText(DiaryFile);
+                return JsonConvert.DeserializeObject<Diary>(jsonData);
+            }
+            return new Diary();
+        }
+    }
+}
---
+++ 18/Task1/NewFolder1/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace StudentDiary
+{
+    public class NotificationService
+    {
+        private const string MapName = "NewAssignmentNotification";
+
+        public static void SendNotification(string message)
+        {
+            using (var mmf = MemoryMappedFile.CreateOrOpen(MapName, 1024))
+            using (var accessor = mmf.CreateViewAccessor())
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                accessor.WriteArray(0, data, 0, data.Length);
+            }
+        }
+
+        public static string ReceiveNotification()
+        {
+            using (var mmf = MemoryMappedFile.OpenExisting(MapName))
+            using (var accessor = mmf.CreateViewAccessor())
+            {
+                byte[] data = new byte[1024];
+                accessor.ReadArray(0, data, 0, data.Length);
+                return Encoding.UTF8.GetString(data).TrimEnd('\0');
+            }
+        }
+    }
+}
